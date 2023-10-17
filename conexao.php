9f4ef3bc8aade75235be6c3d71d7660bad246344
ig0r <ig0r@pop-os.localdomain>
2023-10-17 11:53:13 -0300

    app

--- conexao.php
+++ conexao.php
@@ -7,9 +7,14 @@ $dbname = "celke";
 $port = 3306;
 
 try{
+    //conexão com a porta
+    //$conn = new PDO("mysql:host=$host;port=$port;dbname" . $dbname; $user, $pass);
     $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+    
+    //conexão sem a porta
+    //$conn = new PDO("mysql:host=$host;dbname" . $dbname, $user, $pass);
 
-    echo "conexão com o banco de dados realizado com sucesso!";
+    //echo "conexão com o banco de dados realizado com sucesso!";
 } catch(PDOException $err){
     echo "Erro: Conexão com banco de dados não foi realizado. Erro gerado ". $err->getMessage();
 }
