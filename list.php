dec94d075f6be9f336255a1a67a8b734e85ac03e
igusil <igusil@outlook.com>
2023-10-18 12:03:10 -0300

    pagination func

--- list.php
+++ list.php
@@ -6,7 +6,7 @@ $pagina = filter_input(INPUT_GET, "pagina", FILTER_SANITIZE_NUMBER_INT);
 if (!empty($pagina)){
 
   //calcular o inicio visualização
-  $qnt_result_pg = 10; //Quantidade de registro por pagina
+  $qnt_result_pg = 5; //Quantidade de registro por pagina
   $inicio = ($pagina * $qnt_result_pg) - $qnt_result_pg;
   // 1 * 10 = 10 - 10 = 0
 
@@ -36,7 +36,27 @@ $dados .= "</tbody>
   </table>
 </div>";
 
-echo $dados;
+//paginação - Somar a quantidade de usuarios
+$query_pg = "SELECT COUNT(id) AS num_result FROM usuarios";
+$result_pg = $conn->prepare($query_pg);
+$result_pg->execute();
+$row_pg = $result_pg->fetch(PDO::FETCH_ASSOC);
+
+//Quantidade de paginas
+$quantidade_pg = ceil($row_pg['num_result'] / $qnt_result_pg);
+//16 / 10
+
+
+  $dados .= "<nav aria-label='Page navigation'><ul class='pagination justify-content-center'>";
+  $dados .= "<li class='page-item disabled'><a class='page-link'>Anterior</a></li>";
+  $dados .= "<li class='page-item'><a class='page-link' href='#'>1</a></li>";
+  $dados .= "<li class='page-item'><a class='page-link' href='#'>2</a></li>";
+  $dados .= "<li class='page-item'><a class='page-link' href='#'>3</a></li>";
+  $dados .= "<li class='page-item'><a class='page-link' href='.$quantidade_pg.'>Proximo '$quantidade_pg'</a></li>";
+  
+  $dados .= "</ul></nav>";
+
+  echo $dados;
 
 } else {
   echo"<div class='alert alert-danger' role='alert'>Erro: Nenhum usuario encontrado!</div";
