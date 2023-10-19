31899573999c0382d5a8cd09fe07a5a852e1fb9d
igusil <igusil@outlook.com>
2023-10-18 22:03:20 -0300

    pagination func

--- index.php
+++ index.php
@@ -15,23 +15,54 @@ include_once "conexao.php"
 <body>
     <div class="container">
         <div class="row mt-4">
-            <div class="col-lg-12">
+            <div class="col-lg-12 d-flex justify-content-between align-items-center">
                 <div>
                 <h4>Listar Usuários</h4>
+                </div>
+                <div>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#cadUsuarioModal">Cadastar</button>
                 </div>
             </div>
         </div>
         <hr>
-
         <div class="row">
             <div class="col-lg-12">
-
                 <span class="listar-usuarios"></span>   
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="cadUsuarioModal" tabindex="-1" aria-labelledby="cadUsuarioModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="cadUsuarioModalLabel">Cadastrar Usuário</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <form id="cad-usuario-form">
+                    <div class="mb-3">
+                        <label for="nome" class="col-form-label">Nome:</label>
+                        <input type="text" name="nome" class="form-control" id="nome" placeholder="Digite o nome completo" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="col-form-label">E-mail:</label>
+                        <input type="text" name="email" class="form-control" id="email" placeholder="Digite o seu melhor e-mail" required>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Fechar</button>
+                        <input type="submit" class="btn btn-outline-primary btn-sm" id="cad-usuario-btn" value="Cadastrar"/>
+                    </div>
+                </form>
+
+                </div>
                 
             </div>
         </div>
-        
     </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="./js/custom.js"></script>
 
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 const tbody = document.querySelector(".listar-usuarios");
+const cadForm = document.getElementById("cad-usuario-form");
 
 const listarUsuarios = async (pagina) => {
   const dados = await fetch("./list.php?pagina=" + pagina);
@@ -7,3 +8,15 @@ const listarUsuarios = async (pagina) => {
 }
 
 listarUsuarios(1);
+
+cadForm.addEventListener("submit", async(e) => {
+  e.preventDefault();
+  
+  const dadosForm = new FormData(cadForm);
+  dadosForm.append("add", 1);
+  
+  await fetch("cadastrar.php", {
+    method:"POST",
+    body: dadosForm,
+  });
+});
