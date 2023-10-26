baf261e58a0064b606ef8da4908fbc66d824cbca
J. Igor <89313216+igusil@users.noreply.github.com>
2023-10-25 22:53:09 -0300

    Update celke.sql

--- celke.sql
+++ celke.sql
@@ -6,7 +6,7 @@
 -- Generation Time: Oct 26, 2023 at 03:39 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
-
+--
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
