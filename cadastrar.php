c3004f09241018c8676df05bfd609b941b68ad23
igusil <igusil@outlook.com>
2024-01-04 20:53:59 -0300

    cadP8p

--- cadastrar.php
+++ cadastrar.php
@@ -4,16 +4,24 @@ include_once "conexao.php";
 
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-$query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email)";
-$cad_usuario = $conn->prepare($query_usuario);
-$cad_usuario->bindParam(':nome', $dados['nome']);
-$cad_usuario->bindParam(':email', $dados['email']);
-$cad_usuario->execute();
+if(empty($dados['nome'])){
 
-if($cad_usuario->rowCount()){
-  $retorna= ['erro' => false, 'msg' => "<div class='alert alert-sucess' role='alert'>use successfully cadastration!</div>]";
-}else {
-  $retorna = ['erro' => true, 'msg' => "Erro: Usuário não cadastrado!"];
+}else if(empty($dados['email'])){
+
+}else{
+  $query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email)";
+  $cad_usuario = $conn->prepare($query_usuario);
+  $cad_usuario->bindParam(':nome', $dados['nome']);
+  $cad_usuario->bindParam(':email', $dados['email']);
+  $cad_usuario->execute();
+
+  if($cad_usuario->rowCount()){
+    $retorna= ['erro' => false, 'msg' => "<div class='alert alert-success' role='alert'>
+    Usuário cadastrado com sucesso!</div>"];
+  } else {
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Usuário não cadastrado!</div>"];
+  }
 }
+
 
 echo json_encode($retorna);
